f982fde55211576d6c75fdf7120321a4e9ef483d
Cesar <cesardmn@gmail.com>
2020-06-18 19:20:05 -0300

    create Enter model

---
+++ js/models/Enter.js
@@ -0,0 +1,28 @@
+class Enter {
+  constructor() {
+    this._section = document.querySelector('.enter-section')
+    this._form = this._section.querySelector('form')
+    this._input = this._section.querySelector('input')
+    this._button = this._section.querySelector('button')
+    this._modal = this._section.querySelector('a')
+
+    Object.freeze(this)
+  }
+
+  get section() {
+    return this._section
+  }
+  get form() {
+    return this._form
+  }
+  get input() {
+    return this._input
+  }
+  get button() {
+    return this._button
+  }
+  get modal() {
+    return this._modal
+  }
+
+}
