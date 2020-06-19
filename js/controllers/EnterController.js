ca12b986f2d7553a30cc7d33e7da427db3b86608
Cesar <cesardmn@gmail.com>
2020-06-19 02:34:28 -0300

    create doPost controller

--- js/controllers/EnterController.js
+++ js/controllers/EnterController.js
@@ -1,23 +1,31 @@
-class EnterController{
-  constructor(enter){
+class EnterController {
+  constructor(enter) {
     this._enter = enter
     Object.freeze(this)
   }
-  
+
   loadSpreadsheet() {
-    
-    this._enter.button.setAttribute('disabled', true)    
+
+    this._enter.button.setAttribute('disabled', true)
     this._enter.section.setAttribute('hidden', true)
 
     // show spinner section
 
-    // call api
-    // render response
-    
+    this._doPost(this._enter.input.value)
+
+
     this._enter.button.removeAttribute('disabled')
     this._enter.input.value = ''
-    
+
     // hidden spinner
     // show Spreadsheet
+  }
+
+  async _doPost(ssid) {
+    let spreadsheet = await Service.read(ssid)
+    let response = await spreadsheet.json()
+    console.log(response);
+
+    // render response
   }
 }
